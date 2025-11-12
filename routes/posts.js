1437c6e067f0e55afd6d92437c093d4dc3da038f
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-11 20:47:16 -0500

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// Route for /posts
+
+
 router.get('/', (req, res) => {
   res.send('Posts Page');
 });
 
+// Route for /posts/new
+
+
 router.get('/new', (req, res) => {
   res.send('Create a new post');
 });
-
 module.exports = router;
