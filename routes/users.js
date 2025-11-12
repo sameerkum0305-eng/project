350cbfdd0fe2f749093f87acd3fef67833b85cb6
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-11 20:42:01 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// Route for /users
 router.get('/', (req, res) => {
   res.send('Users Page');
 });
 
-// Route for /users/new
 router.get('/new', (req, res) => {
   res.send('Create a new user');
 });
