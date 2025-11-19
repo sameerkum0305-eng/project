ab7e5cc16a30be6df8fb2a95890d690a2faeec55
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-19 13:45:01 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,32 @@
-const express = 
-require('express');
+const express = require('express');
 
-const router = 
-express.Router(); 
+const router = express.Router(); 
 
 router.get('/', (req, res)=>{
     res.render(`users/list`, {users:users})
 });
+
 router.get('/new', (req, res)=>{
-    res.send('New User Form');
+    res.render('users/new', {firstName: "Please enter your name"});
 });
+
 router.post('/', (req, res)=>{
     const firstName = req.body.firstName;
-    const isValid = firstName !=="";//Check if they are there, added
+    const lastName = req.body.lastName;
+    const age = req.body.age;
+    const gender = req.body.gender;
+
+    const isValid = firstName !=="" && lastName !==""; //Check if they are there, added
     if(isValid)
     {
         console.log(`Adding User: ${firstName}`);
-        users.push({name:firstName});
+        users.push({
+            name:firstName,
+            lastName:lastName,
+            gender:gender,
+            age:age,
+        });
+
         console.log(`New Set of Users: ${users}`);
         res.send("User Created!");
     }
@@ -25,21 +35,26 @@ router.post('/', (req, res)=>{
         res.render("users/new", {firstName:firstName});
     }
 });
-// router.get('/:id',(req, res)=>{
-// 	res.send(`Getting User data: ${req.params.id}`);
-// });
-router.route("/:id").get((req, res)=>{
+
+router.route("/:id")
+.get((req, res)=>{
  	res.send(`Getting User data: ${req.params.id}`);
-}).delete((req, res)=>{
+})
+.delete((req, res)=>{
     res.send(`Deleting user with id: ${req.params.id}`);
-}).put((req, res)=>{
+})
+.put((req, res)=>{
     res.send(`Updating user with id: ${req.params.id}`);
 });
-const users =[{name:"George"},{name:"Justyna"}];
 
+const users =[
+    {name:"George"},
+    {name:"Justyna"}
+];
 
 router.param("id", (req, res, next, id)=>{
     console.log(`Accessing user #${id}`);
     next();
 });
+
 module.exports = router;
