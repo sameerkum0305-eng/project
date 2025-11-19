568d95c7740ed0be9043f018b1d5b23bff8f4f2e
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-19 13:14:08 -0500

    Made sure server.js covers everything we had to type in class

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
+
 const userRouter = require('./routes/users');
 const postRouter = require('./routes/posts');
 
-const app = express();
+const app = express(); 
 app.set('view engine', 'ejs');
-
 app.use(express.static("public"));
-app.use(express.urlencoded((extended:true}));
+app.use(express.urlencoded({extended:true}));
 
 app.use('/users', userRouter);
-app.use('/posts', postRouter);
+app.use('/posts',postRouter);
 
-app.get('/', (req, res) => {
-  console.log('Here');
-  res.render('index', { user: 'Sameer!' });
+app.get('/', (req, res)=>{
+    console.log('Here');
+    res.render("index", {user:"Sameer",});
+}); 
+app.get('/new', (req, res)=>{
+    res.render('users/new', {firstName: "Please enter your name"});
 });
 
-app.get('/new, (req, res)=>{
-        res.render('users/new', {firstName: "Please enter your name:});
-          
+
 app.listen(3030);
+
+function logger(req, res, next){
+    console.log(`Page Accessed: ${req.originalUrl}`);
+    next();
+}
